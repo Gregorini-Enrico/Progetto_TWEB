b6383585459255241fb3bc7ebada4bdf6d347459
Giova <giovanni.giacometti5@gmail.com>
2021-05-15 17:02:53 +0200

    Add year and month to searchbar

--- app/Models/PublicAppModel.php
+++ app/Models/PublicAppModel.php
@@ -13,6 +13,57 @@ class PublicAppModel{
         return Evento::all();
     }
     
+    public function getYears(){
+        $events = Evento::all();
+        $i = 0;
+        $anni = [];
+        foreach ($events as $e){
+            $find = false;
+            
+            //$month=date("F",$this->data);
+            $year=(explode("-", $e->data))[0];
+            Log::debug($year);
+            foreach ($anni as $y){
+                if ($year == $y){
+                    $find = true;
+                    break;
+                }
+            }
+            if ($find == false){
+                    $anni[$i] = $year;
+                    $i = $i + 1;    
+            }
+        }
+        return $anni;
+    }
+
+    
+    public function getMonths(){
+        $events = Evento::all();
+        $i = 0;
+        $mesi = [];
+        foreach ($events as $e){
+            $find = false;
+          
+            //$month=date("F",$this->data);
+            $month=(explode("-", $e->data))[1];
+            Log::debug($month);
+            foreach ($mesi as $m){
+                if ($month == $m){
+                    $find = true;
+                    break;
+                }
+            }
+            if ($find == false){
+                    $mesi[$i] = $month;
+                    $i = $i + 1;    
+            }
+        }
+        Log::debug($mesi);
+        return $mesi;
+    }
+        
+   
     public function getSocieties(){
         return Account::where('livello', 3)->get();
     }
