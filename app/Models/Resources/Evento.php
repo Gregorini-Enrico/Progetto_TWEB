e396c610039800552d01dd09f28d61f4eea17756
Enrico Gregorini <s1090158@studenti.univpm.it>
2021-05-16 11:28:56 +0200

    Fixed method getDiscount() in app\Models\Resources\Evento.php

--- app/Models/Resources/Evento.php
+++ app/Models/Resources/Evento.php
@@ -13,7 +13,8 @@ class Evento extends Model {
 
     public function getDiscount() {
         $price = $this->costo_biglietto;
-        if (($this->data-$this->giorni_sconto)  > now()->toDateTimeString('Y-m-d')) {
+        //verifica che la data di inizio sconto non sia "maggiore" della data attuale nello stesso formato ('Y-m-d')
+        if ($this->data_inizio_sconto  >= now()->toDateTimeString('Y-m-d')) { 
             $discount = ($price * $this->sconto) / 100;
             $price = round($price - $discount, 2);
         }
